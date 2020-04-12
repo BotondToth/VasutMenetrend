22381210ff51a52e2aff13b275037b773bb712e7
Tóth Botond <h675427@stud.u-szeged.hu>
2020-04-12 14:57:59 +0200

    initial commit

---
+++ src/main/java/com/database/szte/VasutInfoApplication.java
@@ -0,0 +1,13 @@
+package com.database.szte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VasutInfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VasutInfoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/database/szte/VasutInfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.database.szte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VasutInfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
